a38525c4fce03f4c97dd7460d4d6c09394852307
Nigar Sultana Kana <nigarkana@gmail.com>
2025-03-29 09:33:18 -0700

    String demo added

---
+++ StringDemo.py
@@ -0,0 +1,11 @@
+
+
+str1 = "Mahid is sleeping"
+str2 = "Mahid has a sound and good dream"
+str3 = "Mahid has"
+print(str1[1])
+print(str1 + "," +" "+ str2)
+
+print(str3 in str2) #to check the string is present or not in the main string
+
+
