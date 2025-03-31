a82fcc1177319b93ff6cbc18ba6366b84215551c
Nigar Sultana Kana <nigarkana@gmail.com>
2025-03-31 11:23:32 -0700

    String Demo added

--- StringDemo.py
+++ StringDemo.py
@@ -2,10 +2,16 @@
 
 str1 = "Mahid is sleeping"
 str2 = "Mahid has a sound and good dream"
-str3 = "Mahid has"
+str3 = "Mahid&has"
 print(str1[1])
 print(str1 + "," +" "+ str2)
 
 print(str3 in str2) #to check the string is present or not in the main string
+
+splitTest = str3.split("&")
+print(splitTest[0])
+str4 = " Masud "
+
+print(str4.strip()) #lstrip and rstrip means to remove space from left and right
 
 
