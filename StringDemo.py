d5a9f3afb4fd354e5c834de9759bd92becda6eda
Nigar Sultana Kana <nigarkana@gmail.com>
2025-03-31 12:38:30 -0700

    More code added in the StringDemo

--- StringDemo.py
+++ StringDemo.py
@@ -6,7 +6,13 @@ str3 = "Mahid&has"
 print(str1[1])
 print(str1 + "," +" "+ str2)
 
+print(str1[3])
+print("String range printing Mahid to Mah:", str1[:3])
+print("String range printing for index 1 to index 7: ", str1[1:7])
 print(str3 in str2) #to check the string is present or not in the main string
+
+str1.insert(6, "Shah")
+print(str1)
 
 splitTest = str3.split("&")
 print(splitTest[0])
